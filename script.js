443703c9f1ddb50cc21026d33e276d15cea08094
Ray Villalobos <ray@planetoftheweb.com>
2022-08-03 23:41:28 +0000

    finished 01_05

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 var temp = 86
-tempC()
-console.log(temp) // 86
 function tempC() {
+    var temp
     console.log(temp) // undefined
     var temp = 50
-    temp = (temp - 32) / 1.8 // 10
-    console.log(temp) // 18
+    temp = (temp - 32) / 1.8
+    console.log(temp) // 10
 }
+tempC()
+console.log(temp) // 86
