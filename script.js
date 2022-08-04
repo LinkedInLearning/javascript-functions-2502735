0e27f4ee51fde73952d5160223938fcd17efe6fa
Ray Villalobos <ray@planetoftheweb.com>
2022-08-04 02:44:42 +0000

    Finished 01_08

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ info = {
   }
 }
 
-function listObject(data) {}
+function listObject(data) {
+  const $ = document.querySelector.bind(document)
+
+  let output = '<ul>'
+  for (const key in data) {
+    output +=`<li><a href="${data[key]}">${key}</a>`
+  }
+  output +='</ul>'
+  $('#output').innerHTML = output
+}
 
 listObject(info.social)
