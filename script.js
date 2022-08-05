d520d2ac3071ca561aaff319f0a9958f7541a06d
Ray Villalobos <ray@planetoftheweb.com>
2022-08-05 02:58:41 +0000

    02_01e

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-function add(...theArgs) {
-  let argSum = theArgs.reduce((sum, val) => sum + val)
-  return argSum;
-}
-output = add(1,2,3,4,5);
-console.log(output) //15
+console.log([1,2,3,4,5].reduce((sum, val) => sum + val))
