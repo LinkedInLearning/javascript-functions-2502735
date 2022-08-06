251cc566a0ecb233fd5885bab4f294ed9f5f3393
Ray Villalobos <ray@planetoftheweb.com>
2022-08-06 19:07:50 +0000

    02_02e

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
+let speak = function(what) {
+  return (console.log(what));
+}
+  
 let Dog = function() {
   let name;
 }
-Dog.prototype = {
-  speak: function(what) {
-    return (console.log(what));
-  }
+
+let Cat = function() {
+  let name;
 }
-let firstDog = new Dog;
-firstDog.name = "Mojo"
-firstDog.speak('woof')
+
+Dog.prototype.speak = speak
+Cat.prototype.speak = speak
+
+let myDog = new Dog;
+let myCat = new Cat;
+
+myDog.name = "Mojo"
+myDog.speak('woof')
+
+myCat.name = "Sniggles"
+myCat.speak('meow')
