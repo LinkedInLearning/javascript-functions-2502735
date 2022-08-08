25ca515299d65f2aaf13d260e84ebe64d56543e1
Ray Villalobos <ray@planetoftheweb.com>
2022-08-08 07:16:04 +0000

    02_08

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function output(data) {
+async function output(data) {
     const $ = document.querySelector.bind(document)
     let output = '<nav><ul>'
     for (const key in data) {
@@ -7,3 +7,16 @@ function output(data) {
     output +='</ul></nav>'
     $('#output').innerHTML = output
 }
+
+async function getSocial() {
+    const res = await fetch('./social.json')
+    const data = await res.json()
+    return data
+}
+
+async function doSocial() {
+    const social = await getSocial()
+    await output(social)
+}
+
+doSocial();
